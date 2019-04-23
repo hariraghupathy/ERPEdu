c4af688f51c21ac974ef6901b0e75fe36ea4103d
Hari Krishnan <hariraghupathy@gmail.com>
2019-04-23 22:12:36 +0530

    ->adding material design to the application ->documentation for issues faced during development

--- WebApplication/ERPEdu/src/app/app.module.ts
+++ WebApplication/ERPEdu/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
