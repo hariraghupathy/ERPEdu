71062626a87a7592f9b3f272a0e3c1103a3ebcd9
Hari Krishnan <hariraghupathy@gmail.com>
2019-04-26 00:29:26 +0530

    ->individual components for management,employee,student,home page are added ->also sub components such as header,footer and home page are added for the home page inside the home page folder ->documentation for todays task added

--- WebApplication/ERPEdu/src/app/app.module.ts
+++ WebApplication/ERPEdu/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { HomePageComponent } from '../home-page/home-page.component';
+import { ManagementComponent } from '../management/management.component';
+import { EmployeeComponent } from '../employee/employee.component';
+import { StudentComponent } from '../student/student.component';
+import { HeaderComponent } from '../home-page/header/header.component';
+import { ContentComponent } from '../home-page/content/content.component';
+import { FooterComponent } from '../home-page/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    HomePageComponent,
+    ManagementComponent,
+    EmployeeComponent,
+    StudentComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
